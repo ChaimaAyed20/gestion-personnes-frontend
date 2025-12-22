b9615d033921e53bdcd9bb0afbe1f28c00211616
ChaimaAyed20 <ayedchaima00@gmail.com>
2025-12-22 11:11:29 +0100

    Initial commit: React + Vite + Bootstrap setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">Gestion des Personnes - Frontend React</h1>
+      <p className="text-center">Projet prêt !</p>
+    </div>
+  );
+}
+
+export default App;
